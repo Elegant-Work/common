20e49e5b9505248e20ce9a3b2332b1eebd0415e0
Juanco Morton <Juanco.Morton@icloud.com>
2024-02-12 13:08:07 +0200

    Add composer.lock and update arrays.php***

--- src/arrays.php
+++ src/arrays.php
@@ -9,15 +9,17 @@ class arrays
 {
 
 	//Debugging output
-	public static function pp(array $Array, bool $Return = false): ?string
+	public static function pp(array $Array, bool $Return = false): string|null
 	{
 		if ($Return) {
 			return "<pre>" . print_r($Array, true) . "</pre>";
 		}
 
 		echo "<pre>";
-		echo print_r($Array, false);
+		echo print_r($Array);
 		echo "</pre>";
+
+		return null;
 	}
 
 }
